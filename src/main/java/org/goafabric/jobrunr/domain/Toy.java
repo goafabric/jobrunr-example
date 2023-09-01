36c9e6116dfbb10985e8f4856fb88dd178106449
Andreas Mautsch <amautsch>
2023-09-01 13:15:24 +0200

    changes

--- src/main/java/org/goafabric/jobrunr/domain/Toy.java
+++ src/main/java/org/goafabric/jobrunr/domain/Toy.java
@@ -1,13 +1,13 @@
 package org.goafabric.jobrunr.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("toy_catalog")
 public record Toy (
-    @Id
-    String id,
-    String version,
+    @Id String id,
+    @Version String version,
     String toyName,
     String price
 ) {}
