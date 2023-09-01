4fff4c75898decb1c7fc76c06fb8977d15396524
Andreas Mautsch <amautsch>
2023-09-01 10:16:08 +0200

    changes

--- src/main/java/org/goafabric/jobrunr/Application.java
+++ src/main/java/org/goafabric/jobrunr/Application.java
@@ -1,10 +1,13 @@
 package org.goafabric.jobrunr;
 
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 
 /**
@@ -12,6 +15,7 @@ import org.springframework.context.annotation.Bean;
  */
 
 @SpringBootApplication
+@ImportRuntimeHints(Application.ApplicationRuntimeHints.class)
 public class Application {
 
     public static void main(String[] args) throws Exception {
@@ -23,5 +27,11 @@ public class Application {
         return args -> {if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {SpringApplication.exit(context, () -> 0);}};
     }
 
+    static class ApplicationRuntimeHints implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.resources().registerPattern("catalogdata/*.csv");
+        }
+    }
 
 }
--- src/main/java/org/goafabric/jobrunr/job/launcher/JobLauncher.java
+++ src/main/java/org/goafabric/jobrunr/job/launcher/JobLauncher.java
@@ -2,6 +2,7 @@ package org.goafabric.jobrunr.job.launcher;
 
 import org.goafabric.jobrunr.job.simple.SimpleJob;
 import org.jobrunr.scheduling.BackgroundJob;
+import org.jobrunr.scheduling.JobScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,11 +13,16 @@ public class JobLauncher implements CommandLineRunner {
     @Autowired
     private SimpleJob simpleJob;
 
+    @Autowired
+    private JobScheduler jobScheduler;
+
     @Override
     public void run(String... args) throws Exception {
         //jobScheduler.enqueue(() -> System.out.println("Up & Running from a background Job"));
 
         //jobScheduler.enqueue(() -> simpleJob.run());
         BackgroundJob.enqueue(() -> simpleJob.run());
+
+        //jobScheduler.create(JobBuilder.aJob().withName("test").withAmountOfRetries(3).withDetails(() -> simpleJob.run()));
     }
 }
