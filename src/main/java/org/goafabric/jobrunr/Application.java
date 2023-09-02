cdb4ee681e04b01dcc55fe033590f1952b29fcae
Andreas Mautsch <amautsch>
2023-09-02 13:14:55 +0200

    native changes

--- src/main/java/org/goafabric/jobrunr/Application.java
+++ src/main/java/org/goafabric/jobrunr/Application.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
 @ImportRuntimeHints(Application.ApplicationRuntimeHints.class)
+//@EnableJdbcRepositories(considerNestedRepositories = true)
 public class Application {
 
     public static void main(String[] args) throws Exception {
