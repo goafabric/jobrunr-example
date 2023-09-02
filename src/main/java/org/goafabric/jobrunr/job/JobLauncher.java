7d8386b287143560966ec1ea837995a45ce98a53
Andreas Mautsch <amautsch>
2023-09-02 14:27:24 +0200

    refactoring

--- src/main/java/org/goafabric/jobrunr/job/JobLauncher.java
+++ src/main/java/org/goafabric/jobrunr/job/JobLauncher.java
@@ -2,8 +2,12 @@ package org.goafabric.jobrunr.job;
 
 import org.goafabric.jobrunr.job.person.PersonJobRequest;
 import org.goafabric.jobrunr.job.toy.ToyJobRequest;
+import org.jobrunr.jobs.mappers.JobMapper;
 import org.jobrunr.scheduling.BackgroundJobRequest;
+import org.jobrunr.storage.InMemoryStorageProvider;
+import org.jobrunr.storage.StorageProvider;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +18,13 @@ public class JobLauncher implements CommandLineRunner {
         //BackgroundJob.enqueue(() -> simpleJob.run());
         BackgroundJobRequest.enqueue(new PersonJobRequest());
         BackgroundJobRequest.enqueue(new ToyJobRequest());
+    }
+
+    @Bean
+    public StorageProvider storageProvider(JobMapper jobMapper) {
+        InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
+        storageProvider.setJobMapper(jobMapper);
+        return storageProvider;
     }
 
 }
