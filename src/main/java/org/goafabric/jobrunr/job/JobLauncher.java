66f4f81ed855d1dee15b0ba169a79e441ce5fe3d
Andreas Mautsch <amautsch>
2023-09-02 09:19:43 +0200

    changes

--- src/main/java/org/goafabric/jobrunr/job/JobLauncher.java
+++ src/main/java/org/goafabric/jobrunr/job/JobLauncher.java
@@ -2,6 +2,7 @@ package org.goafabric.jobrunr.job;
 
 import org.goafabric.jobrunr.job.simple.SimpleJob;
 import org.goafabric.jobrunr.job.toy.ToyCatalogJob;
+import org.jobrunr.scheduling.BackgroundJob;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -21,7 +22,7 @@ public class JobLauncher implements CommandLineRunner {
         //jobScheduler.create(JobBuilder.aJob().withName("test").withAmountOfRetries(3).withDetails(() -> simpleJob.run()));
 
         //BackgroundJob.enqueue(() -> simpleJob.run());
-        //BackgroundJob.enqueue(toyCatalogJob.reader(), toy -> toyCatalogJob.writer(toy));
+        BackgroundJob.enqueue(toyCatalogJob.reader(), toy -> toyCatalogJob.writer(toy));
 
     }
 }
