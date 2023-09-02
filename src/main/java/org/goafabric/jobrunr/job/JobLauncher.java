6da74d0af0fcb2b2119b91958fa003fa0a1da17f
Andreas Mautsch <amautsch>
2023-09-02 10:57:42 +0200

    schema changes

--- src/main/java/org/goafabric/jobrunr/job/JobLauncher.java
+++ src/main/java/org/goafabric/jobrunr/job/JobLauncher.java
@@ -3,13 +3,9 @@ package org.goafabric.jobrunr.job;
 import org.goafabric.jobrunr.job.easy.SimpleJob;
 import org.goafabric.jobrunr.job.person.PersonJob;
 import org.goafabric.jobrunr.job.toy.ToyJob;
-import org.jobrunr.jobs.mappers.JobMapper;
 import org.jobrunr.scheduling.BackgroundJob;
-import org.jobrunr.storage.InMemoryStorageProvider;
-import org.jobrunr.storage.StorageProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -27,14 +23,16 @@ public class JobLauncher implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         //BackgroundJob.enqueue(() -> simpleJob.run());
-        //BackgroundJob.enqueue(toyJob.reader(), toy -> toyJob.writer(toy));
+        BackgroundJob.enqueue(toyJob.reader(), toy -> toyJob.writer(toy));
         BackgroundJob.enqueue(personJob.reader(), person -> personJob.writer(person));
     }
 
+    /*
     @Bean
     public StorageProvider storageProvider(JobMapper jobMapper) {
         InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
         storageProvider.setJobMapper(jobMapper);
         return storageProvider;
     }
+    */
 }
