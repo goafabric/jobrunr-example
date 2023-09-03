309ac07f60e73f4b92d97ad800da283245e47ab8
Andreas Mautsch <amautsch>
2023-09-03 16:49:08 +0200

    changes

--- src/main/kotlin/org/goafabric/jobrunr/job/JobLauncher.kt
+++ src/main/kotlin/org/goafabric/jobrunr/job/JobLauncher.kt
@@ -11,8 +11,9 @@ import java.time.Instant
 class JobLauncher : CommandLineRunner {
     @Throws(Exception::class)
     override fun run(vararg args: String) {
-        //BackgroundJob.enqueue(() -> new SimpleJob().run());  //simple pojo
+
         BackgroundJobRequest.enqueue(PersonJobRequest()) //jobrequest needed for native images
+
         BackgroundJobRequest.schedule(Instant.now().plusSeconds(2), ToyJobRequest()) //scheduler
     }
 }
