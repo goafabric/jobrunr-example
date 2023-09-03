ac75876f2476df518c7999d73f6bbe54ebfca039
Andreas Mautsch <amautsch>
2023-09-03 09:12:01 +0200

    changes

--- src/main/java/org/goafabric/jobrunr/job/JobLauncher.java
+++ src/main/java/org/goafabric/jobrunr/job/JobLauncher.java
@@ -3,13 +3,9 @@ package org.goafabric.jobrunr.job;
 import org.goafabric.jobrunr.job.easy.SimpleJob;
 import org.goafabric.jobrunr.job.person.PersonJobRequest;
 import org.goafabric.jobrunr.job.toy.ToyJobRequest;
-import org.jobrunr.jobs.mappers.JobMapper;
 import org.jobrunr.scheduling.BackgroundJob;
 import org.jobrunr.scheduling.BackgroundJobRequest;
-import org.jobrunr.storage.InMemoryStorageProvider;
-import org.jobrunr.storage.StorageProvider;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
@@ -23,17 +19,20 @@ public class JobLauncher implements CommandLineRunner {
 
         BackgroundJobRequest.enqueue(new PersonJobRequest()); //jobrequest needed for native images
 
-        BackgroundJobRequest.schedule(Instant.now().plusSeconds(20), new ToyJobRequest()); //scheduler
+        BackgroundJobRequest.schedule(Instant.now().plusSeconds(2), new ToyJobRequest()); //scheduler
 
         //BackgroundJob.scheduleRecurrently(Cron.lastBusinessDayOfTheMonth(10, 30),
           //      () -> System.out.println("Last business day of the month!"));
     }
 
+    //in memory storage currently needs to be enabled for native image support
+    /*
     @Bean
     public StorageProvider storageProvider(JobMapper jobMapper) {
         InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
         storageProvider.setJobMapper(jobMapper);
         return storageProvider;
     }
+    */
 
 }
