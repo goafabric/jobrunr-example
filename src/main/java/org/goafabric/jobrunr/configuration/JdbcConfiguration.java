b0e909595f279a34edd0eb64d312677781ef8ca8
Andreas Mautsch <amautsch>
2023-09-17 11:22:00 +0200

    profile changes

--- src/main/java/org/goafabric/jobrunr/configuration/JdbcConfiguration.java
+++ src/main/java/org/goafabric/jobrunr/configuration/JdbcConfiguration.java
@@ -4,12 +4,12 @@ import org.goafabric.jobrunr.Application;
 import org.jobrunr.spring.autoconfigure.JobRunrProperties;
 import org.springframework.boot.actuate.autoconfigure.data.mongo.MongoHealthContributorAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @Configuration
-@Profile("jdbc")
+@ConditionalOnProperty(value = "spring.profiles.active", havingValue = "jdbc", matchIfMissing = true)
 @EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class, org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class, org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration.class, MongoHealthContributorAutoConfiguration.class})
 @EnableJdbcRepositories(considerNestedRepositories = true, basePackageClasses = Application.class)
 public class JdbcConfiguration {
--- src/main/java/org/goafabric/jobrunr/configuration/MongoConfiguration.java
+++ src/main/java/org/goafabric/jobrunr/configuration/MongoConfiguration.java
@@ -3,15 +3,15 @@ package org.goafabric.jobrunr.configuration;
 import org.goafabric.jobrunr.Application;
 import org.jobrunr.spring.autoconfigure.JobRunrProperties;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@Profile("mongodb")
+@ConditionalOnProperty(value = "spring.profiles.active", havingValue = "mongodb", matchIfMissing = false)
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, JdbcRepositoriesAutoConfiguration.class})
 @EnableMongoRepositories(considerNestedRepositories = true, basePackageClasses = Application.class)
 public class MongoConfiguration {
