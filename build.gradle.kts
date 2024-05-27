a0b2c3169fb1928f38dcb8701b4905dd509138ae
Andreas Mautsch <amautsch>
2024-05-27 16:00:41 +0200

    upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
 	//web
 	implementation("org.springframework.boot:spring-boot-starter")
 
-	implementation("org.jobrunr:jobrunr-spring-boot-3-starter:6.3.1")
+	implementation("org.jobrunr:jobrunr-spring-boot-3-starter:7.1.2")
 
 	//persistence
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
