e5e6b4cf0f578d70ebda151ed974451bed28e08e
Andreas Mautsch <amautsch>
2025-01-01 17:59:23 +0100

    spring upgrade

--- src/main/java/org/goafabric/jobrunr/Application.java
+++ src/main/java/org/goafabric/jobrunr/Application.java
@@ -1,7 +1,10 @@
 package org.goafabric.jobrunr;
 
+import org.flywaydb.core.internal.publishing.PublishingConfigurationExtension;
+import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.annotation.RegisterReflection;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +19,7 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
 @ImportRuntimeHints(Application.ApplicationRuntimeHints.class)
+@RegisterReflection(classes = PublishingConfigurationExtension.class, memberCategories = MemberCategory.INVOKE_PUBLIC_METHODS)
 public class Application {
 
     public static void main(String[] args) throws Exception {
